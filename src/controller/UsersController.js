b65787923335daa418ad0da72220a13e16e97c3d
Edmilson Henrique Dias Rodrigues <92001139+Edmilsonhdr@users.noreply.github.com>
2022-08-06 13:31:50 -0300

    Add files via upload

--- src/controller/UsersController.js
+++ src/controller/UsersController.js
@@ -1,6 +1,12 @@
+const AppError = require("../Utils/AppError")
+
 class UsersController {
     create(req,res){
         const {name,email,senha}=req.body
+
+        if(!name){
+            throw new AppError("Nome é obrigatorio")
+        }
 
         res.status(201).json({name,email,senha})
     }
